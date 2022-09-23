ad7954df02443fc800d7c6d2aff33650661fed49
DVincent224 <djvincent224@gmail.com>
2022-09-23 12:01:23 -0500

    Test

--- Deck.java
+++
@@ -1,9 +0,0 @@
-public class Deck {
-    private boolean[] deck = new boolean[52];
-
-    public void initDeck(){
-        for(int i=0;i<this.deck.length;i++){
-            this.deck[i] = true;
-        }
-    }
-}
---
+++ src/DeckDriver.java
@@ -0,0 +1,9 @@
+public class DeckDriver {
+    public static void main(String[] args){
+        final int cardsPerRow = 8;
+        int cardsThisRow = 0;
+        int myCard;
+        Deck myDeck = new Deck();
+        myDeck.initDeck();
+    }
+}
