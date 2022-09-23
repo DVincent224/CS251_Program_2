e5a9d39653cad4a0ccc3ace4c9808a5e9ace8898
DVincent224 <djvincent224@gmail.com>
2022-09-23 18:55:34 -0500

    9/23

--- src/DeckDriver.java
+++ src/DeckDriver.java
@@ -5,5 +5,20 @@ public class DeckDriver {
         int myCard;
         Deck myDeck = new Deck();
         myDeck.initDeck();
+        System.out.println("\nHere is a shuffled deck ...\n");
+        while (!(myDeck.emptyDeck())){
+            myCard = myDeck.dealCard();
+            ++cardsThisRow;
+            if (cardsThisRow <= cardsPerRow){
+                System.out.print(Deck.cardToString(myCard));
+                System.out.print(" ");
+            }
+            else{
+                System.out.println("");
+                cardsThisRow = 1;
+                Deck.cardToString(myCard);
+
+            }
+        }
     }
 }
